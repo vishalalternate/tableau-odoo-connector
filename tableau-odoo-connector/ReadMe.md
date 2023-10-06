764e7e4f5827935468449eea7b5f5111b99aa637
vishalalternate <145339268+vishalalternate@users.noreply.github.com>
2023-10-06 16:43:31 +0530

    Add files via upload

---
+++ tableau-odoo-connector/ReadMe.md
@@ -0,0 +1,15 @@
+-----------------------------------
+Welcome to -> https://techneith.com/
+
+To connect Odoo with Tableau you can use this connector
+
+https://techneith.com/products/tableau-direct-connector/
+
+Techneith provides you with complete walk through for the connector and a live demo also.
+
+Feel free to connect - info@techneith.com
+------------------------------------
+
+Interested to buy Odoo to Power BI Connector also
+
+Visit -> https://techneith.com/products/odoo-power-bi-direct-connector/
---
+++ tableau-odoo-connector/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Tableau Connector",
+
+    'summary': """
+       	  Connects your Tableau directly with Odoo. Microsoft Power BI Tableau.
+       """,
+
+    'description': """
+       Connects your Tableau directly with Odoo. Microsoft Power BI Tableau.
+    """,
+        
+    'maintainer': 'Tableau TN',
+    'author': "Tableau TN",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Extra Tools',
+    'version': '1.1',
+    'price': 0,
+    'currency': 'USD',
+    # any module necessary for this one to work correctly
+ 
+    'images': ['static/description/tbl.gif'],
+    "license":"LGPL-3",
+
+    # always loaded
+   
+    # only loaded in demonstration mode
+    "application": True,
+    "installable": True,
+     'data': [
+
+    ],
+   'depends': [],
+}
